c2a43e8ec9633cc39d38861046e9fdf9c43a3903
Omar Muhammad <eng.omarmuhammad@gmail.com>
2026-06-05 11:25:56 +0300

    fix: update caniuse-lite version and add license information in package-lock.json; adjust svgr plugin configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr({ include: "**/*.svg" })],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
