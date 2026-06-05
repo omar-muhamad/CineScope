46fba99f7ab769360bd1d5101638490da29ae3df
Omar Muhammad <eng.omarmuhammad@gmail.com>
2026-06-05 08:27:32 +0300

    fix: update video provider URLs for Player 1 and Player 2

--- src/components/watch/providers.ts
+++ src/components/watch/providers.ts
@@ -15,14 +15,14 @@ export const providers: Provider[] = [
     name: "Player 1",
     build: ({ media_type, id, season, episode }) =>
       media_type === "tv"
-        ? `https://player.videasy.net/tv/${id}/${season}/${episode}`
-        : `https://player.videasy.net/movie/${id}`,
+        ? `https://vidsrc.me/embed/tv/${id}/${season}-${episode}`
+        : `https://vidsrc.me/embed/movie/${id}`,
   },
   {
     name: "Player 2",
     build: ({ media_type, id, season, episode }) =>
       media_type === "tv"
-        ? `https://vidsrc.me/embed/tv/${id}/${season}-${episode}`
-        : `https://vidsrc.me/embed/movie/${id}`,
+        ? `https://player.videasy.net/tv/${id}/${season}/${episode}`
+        : `https://player.videasy.net/movie/${id}`,
   },
 ];
