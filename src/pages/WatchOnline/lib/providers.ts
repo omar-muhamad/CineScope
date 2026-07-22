b61d6ff42c16fd70ac469f5530407a17eec24311
Omar Muhammad <eng.omarmuhammad@gmail.com>
2026-07-22 14:13:59 +0300

    feat: simplify player names and adjust layout in WatchDetailsContent

--- src/pages/WatchOnline/lib/providers.ts
+++ src/pages/WatchOnline/lib/providers.ts
@@ -12,28 +12,28 @@ export type Provider = {
 
 export const providers: Provider[] = [
   {
-    name: "Player 1",
+    name: "P1",
     build: ({ media_type, id, season, episode }) =>
       media_type === "tv"
         ? `https://vsembed.su/embed/tv/${id}/${season}-${episode}`
         : `https://vsembed.su/embed/movie/${id}`,
   },
   {
-    name: "Player 2",
+    name: "P2",
     build: ({ media_type, id, season, episode }) =>
       media_type === "tv"
         ? `https://vidcore.net/tv/${id}/${season}/${episode}`
         : `https://vidcore.net/movie/${id}`,
   },
   {
-    name: "Player 3",
+    name: "P3",
     build: ({ media_type, id, season, episode }) =>
       media_type === "tv"
         ? `https://player.videasy.net/tv/${id}/${season}/${episode}`
         : `https://player.videasy.net/movie/${id}`,
   },
   {
-    name: "Player 4",
+    name: "P4",
     build: ({ media_type, id, season, episode }) =>
       media_type === "tv"
         ? `https://player.cinezo.live/embed/tv/${id}/${season}/${episode}`
